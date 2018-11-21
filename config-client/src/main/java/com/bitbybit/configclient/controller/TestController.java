9db531b30086d95247980e5ed5e3c79993e27760
liulin02 <liulin@koolearn.com>
2018-11-21 20:49:24 +0800

    config  demo  finish

--- config-client/src/main/java/com/bitbybit/configclient/controller/TestController.java
+++ config-client/src/main/java/com/bitbybit/configclient/controller/TestController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "test")
 public class TestController {
 
-    // @Value("${development.foo}")
+    @Value("${liulin}")
     String name = "World";
 
     @RequestMapping("home")
