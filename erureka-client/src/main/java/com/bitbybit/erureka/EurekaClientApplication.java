7adc6dd1a264c5e34db365fda96df3ca229ca2e5
liulin02 <liulin@koolearn.com>
2018-11-22 13:50:25 +0800

    eureka server

---
+++ erureka-client/src/main/java/com/bitbybit/erureka/EurekaClientApplication.java
@@ -0,0 +1,14 @@
+package com.bitbybit.erureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class EurekaClientApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaClientApplication.class, args);
+	}
+}
